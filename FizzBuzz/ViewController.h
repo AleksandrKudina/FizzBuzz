098541eca3ce17a1de223b4e4f52ef8b8863706e
Александр Кудина <aleksandrkudina@mac-aleksandr.local>
2016-08-03 10:58:49 +0300

    Initial Commit

---
+++ FizzBuzz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FizzBuzz
+//
+//  Created by Александр Кудина on 03.08.16.
+//  Copyright © 2016 Aleksandr Kudina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
